c17c087b2e405514b9c1caca4d22dfd2af09b4de
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-12-25 23:02:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-primary h-screen w-screen">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
+
