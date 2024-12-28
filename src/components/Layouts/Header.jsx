d500ee521c705530f138740bcb88cc12f19aac59
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-12-27 23:09:33 -0500

    header logo, titulo y navbar

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Logo } from '../UI/Logo'
+import { Title_header } from '../UI/Title_header'
+import { Navbar } from '../UI/Navbar'
 
 export const Header = () => {
   return (
-    <div className='bg-[#01265A] flex items-center justify-between h-screen'>
+    <div className='bg-primary flex items-center justify-between min-h-32'>
       <Logo/>
+      <Title_header/>
+      <Navbar/>
     </div>
   )
 }
--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import {ItemsNavbar} from "../UI/ItemsNavbar"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center justify-between'>
+       <ul>
+
+        <ItemsNavbar text = "Inicio"/>
+        <ItemsNavbar text = "¿Quien Soy"/>
+        <ItemsNavbar text = "Contactame"/>
+        <ItemsNavbar text = "Proyectos"/>
+        
+       </ul>
+    </nav>
   )
 }
