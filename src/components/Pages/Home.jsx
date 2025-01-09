0b967000f427aec43395d9360a10cdb3a9c6b4d6
Carlos <carlos_aperdomo@soy.sena.edu.co>
2025-01-08 22:06:29 -0500

    home perfil

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import BackGround from "../../assets/img/fondo_home.jpg"
+import BGhome from "../../assets/img/bg_developer.jpg"
+import profilehome from "../../assets/img/developer.jpg"
 
 
 export const Home = () => {
   return (
     <div className='w-screen'> 
-      <img src={BackGround} alt="fondo" />
+      <img src={BGhome} alt="fondo" />
+      <img src={profilehome} alt='perfil' className='absolute left-[60%] top-[250px]'/>
+      <div className='flex-grow justify-around absolute left-[10%] top-[80%] w-[600px] h-[250px] text-white text-[32px]'>
+        <h2>Hola!</h2>
+        <p>
+           Soy Carlos Andres Perdomo Medina <br />
+           tecnólogo y estudiante del SENA 
+           en Análisis y Desarrollo de Software
+       
+        </p>
+      </div>
 
     </div>
   )
